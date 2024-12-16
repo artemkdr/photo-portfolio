dc97bb4148f2b1ffd21d4e1f593621923642d070
artemkdr <artem.kdr@gmail.com>
2024-12-16 17:25:24 +0100

    try to fix github actions build;

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,6 @@ export const appConfig: {
     photosLimit: number;
 } = {
     blobDomain: '26rwtwzj5lztj5sb.public.blob.vercel-storage.com',
-    dataSource: process.env.BLOB_READ_WRITE_TOKEN != null ? 'blob' : 'fs',
+    dataSource: process.env.BLOB_READ_WRITE_TOKEN ? 'blob' : 'fs',
     photosLimit: 300,
 };
