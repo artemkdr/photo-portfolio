9046455329b34e2f0d20ff28938455e9525420e2
artemkdr <artem.kdr@gmail.com>
2025-01-22 08:45:41 +0100

    Add tests for photo fetching and encoding; update fetchPhotosFromBlob to decode and then encode filenames

--- src/data/blob-storage/data.test.ts
+++ src/data/blob-storage/data.test.ts
@@ -17,6 +17,16 @@ describe('fetchPhotosFromBlob', () => {
             uploadedAt: new Date(2023, 2, 1),
         },
         {
+            pathname: 'photos/photo%203.png',
+            url: '/photos/photo%203.png',
+            uploadedAt: new Date(2022, 12, 1),
+        },
+        {
+            pathname: 'photos/фото 4.png',
+            url: '/photos/фото 4.png',
+            uploadedAt: new Date(2022, 11, 1),
+        },
+        {
             pathname: 'photos/document.txt',
             url: '/photos/document.txt',
             uploadedAt: new Date(2023, 1, 1),
@@ -30,7 +40,7 @@ describe('fetchPhotosFromBlob', () => {
 
     it('should fetch photos with valid extensions sorted by upload time', async () => {
         const photos = await fetchPhotosFromBlob();
-        expect(photos).toHaveLength(2);
+        expect(photos).toHaveLength(4);
         expect(photos[0]).toEqual({
             src: '/photos/photo2.png',
             name: 'photo2.png',
@@ -47,9 +57,21 @@ describe('fetchPhotosFromBlob', () => {
 
     it('should ignore files with invalid extensions', async () => {
         const photos = await fetchPhotosFromBlob();
-        expect(photos).toHaveLength(2);
+        expect(photos).toHaveLength(4);
         expect(
             photos.find((photo) => photo.name === 'document.txt')
         ).toBeUndefined();
+    });
+
+    it('should correctly encode file names', async () => {
+        const photos = await fetchPhotosFromBlob();
+        expect(
+            photos.find((photo) => photo.name === 'photo%203.png') // originally 'photo%203.png'
+        ).toBeDefined();
+        expect(
+            photos.find(
+                (photo) => photo.name === '%D1%84%D0%BE%D1%82%D0%BE%204.png'
+            ) // originally 'фото 4.png'
+        ).toBeDefined();
     });
 });
